e580381cefdee99728637f55e12c44344a4b7a6e
Prupke <britt.prop@ziggo.nl>
2022-06-07 21:03:44 +0200

    creating admin page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,14 +1,14 @@
 import Menu from './components/Menu';
 import Header from './components/Header'
 import Toolinformation from './components/Toolinformation';
+import ProfileHeader from './components/ProfileHeader';
 
 function Home() {
     return (
       <div className="Home">
         <Header />
-        {/* <Toolinformation addbtn='Reparatie aanmaken' toolname='Reparaties'/> */}
+        <ProfileHeader title="Hello" secondTitle="Britt Prop" thirdTitle="fijn dat je er weer bent"/>
         <Menu />
-        <h1>hi there this is the home page</h1>
     </div> 
     );
   }
