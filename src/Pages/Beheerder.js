640d0c71de6e4202efc02229fde966b21c4d871c
joeyvonck <j.vonck@hotmail.com>
2022-06-14 17:58:44 +0200

    .

--- src/Pages/Beheerder.js
+++ src/Pages/Beheerder.js
@@ -8,12 +8,12 @@ function Beheerder() {
         <Header />
         <ProfileHeader title="aanwezigheid" secondTitle="beheerder"/>
       <div className='adminCenter'> 
-        <h2 className='spacingTitle'>Profiel gegevens</h2>
+        <h2 className='spacingTitle'>Beheerders informatie</h2>
         <div className='adminInfo'> 
-          <h3 className='spacingTitle'> Joey Vonck </h3>
-          <h3 className='spacingTitle'> Roncalli</h3>
-          <h5> j.vonck@hotmail.com</h5>
-          <h5> tel: 0619628475</h5>
+          <h3 className='spacingTitle'> Enno Overbeeke </h3>
+          <h3 className='spacingTitle'> Beheerder</h3>
+          <h5> Ennooverbeeke@gmail.nl</h5>
+          <h5> tel: 0612345678</h5>
         </div>
       </div>
         <Menu />
